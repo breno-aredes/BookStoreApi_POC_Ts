45e0e4f8116523071cbc27cc6b55629eb208378a
breno-aredes <breno_mg10@hotmail.com>
2023-04-07 10:23:54 -0300

    feat: schema and validateSchema created from sigup route

---
+++ src/middlewares/validateSchema.ts
@@ -0,0 +1,15 @@
+import err from "../errors/index.js";
+import { Request, Response, NextFunction } from "express";
+import { ObjectSchema } from "joi";
+
+export function validateSchema(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      throw err.conflictError(errors);
+    }
+
+    next();
+  };
+}
--- src/routes/user.Routes.ts
+++ src/routes/user.Routes.ts
@@ -1,9 +1,15 @@
 import userControllers from "controllers/userControllers";
 import { Router } from "express";
+import { validateSchema } from "middlewares/validateSchema";
+import { signupSchema } from "schemas/userSchemas";
 
 const userRouter = Router();
 
-userRouter.post("/signup", userControllers.signup);
+userRouter.post(
+  "/signup",
+  validateSchema(signupSchema),
+  userControllers.signup
+);
 userRouter.post("/signup");
 
 export default userRouter;
